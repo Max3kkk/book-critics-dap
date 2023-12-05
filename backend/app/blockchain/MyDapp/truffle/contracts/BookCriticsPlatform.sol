687d8e7fbd6a6bfeb6088595a6cd40abf944d3be
Artur Eremov <ipipos56@bk.ru>
2023-12-05 12:21:52 +0300

    Update BookCriticsPlatform.sol

--- backend/app/blockchain/MyDapp/truffle/contracts/BookCriticsPlatform.sol
+++ backend/app/blockchain/MyDapp/truffle/contracts/BookCriticsPlatform.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./BookCriticsToken.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BookCriticsPlatform is Ownable {
