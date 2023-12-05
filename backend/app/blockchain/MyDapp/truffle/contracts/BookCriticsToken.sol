a1d7b90a2b0a5342420169493fcc324bc2628181
Artur Eremov <ipipos56@bk.ru>
2023-12-05 12:19:36 +0300

    Update BookCriticsToken.sol

--- backend/app/blockchain/MyDapp/truffle/contracts/BookCriticsToken.sol
+++ backend/app/blockchain/MyDapp/truffle/contracts/BookCriticsToken.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract BookCriticsToken is ERC20 {
-    constructor() ERC20("BookCriticsToken", "BCT") {}
+contract BookCriticsToken is ERC20, Ownable {
+    constructor() ERC20("BookCriticsToken", "BCT") Ownable(msg.sender) {}
 
-    function mint(address to, uint256 amount) public {
+    function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 }
